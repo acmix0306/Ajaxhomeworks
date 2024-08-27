1ea12e5f12a95817b38b2613f955cf88bbc91a98
acmix0306 <161919837+acmix0306@users.noreply.github.com>
2024-08-27 22:49:22 +0800

    first commit

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+# render 找 home 資料夾底下的 index.html
+def index(request):
+    return render(request, 'home/index.html') 
+
+# json demo
+def json_demo(request):
+    return render(request, 'home/json.html')
+
+
